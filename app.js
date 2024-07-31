24a321ae6057a82828c2b78be0fab21f75faefd0
Haseeb ur Rehman <haseeburrehman763@gmail.com>
2024-07-31 17:11:52 +0500

    reduce complete

--- app.js
+++ app.js
@@ -80,3 +80,32 @@
 
 // >>>>>>>>>>>>>>>> Reduce >>>>>>>>>>>>>>>>
 
+//basic example
+// const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// let sum = myNums.reduce((acc,currval)=> acc+currval,0)
+// console.log(sum)
+
+//complex example
+
+// const shoppingCart = [
+//     {
+//         itemName: "js course",
+//         price: 2999
+//     },
+//     {
+//         itemName: "py course",
+//         price: 999
+//     },
+//     {
+//         itemName: "mobile dev course",
+//         price: 5999
+//     },
+//     {
+//         itemName: "data science course",
+//         price: 12999
+//     },
+// ]
+
+// const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+// console.log(priceToPay);
